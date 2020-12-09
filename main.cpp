d93832cf3d579d59628dfb9f5c9e769e842095dd
Marisol Mattson <marisolrm2000@gmail.com>
2020-12-09 12:55:25 -0700

    fghj

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <set>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <utility>
+using namespace std;
+int main(int argc, char *argv[])
+{
+    vector<string> tokens;
+    set <string> unique;
+    string next_line;  // Each data line
+    ifstream in(argv[1]);
+    while (getline(in, next_line)) {
+        istringstream iss(next_line);
+        string token;
+        while (iss >> token) {
+            string nopunct = "";
+            for(auto &c : token) { // Remove Punctuation      
+                if (isalpha(c)) {
+                    nopunct +=c;       
+                }
+            }
+	          tokens.push_back(nopunct);
+		        unique.insert(nopunct);
+		    // cout << token<<endl;
+	    }
+    }
+    cout << "Number of words "<<tokens.size()<<endl;
+    cout << "Number of unique words "<<unique.size()<<endl;
+    for (set<string>::iterator it=unique.begin(); it!=unique.end(); ++it)
+        cout << ' ' << *it;
+    cout << endl;
